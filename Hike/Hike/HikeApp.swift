1d81fb8841fc60642056f947fa9151818a0f8d9e
Keri Levesque <kerianne166@gmail.com>
2021-11-06 19:03:36 -0400

    added starter project

---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Keri Levesque on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
